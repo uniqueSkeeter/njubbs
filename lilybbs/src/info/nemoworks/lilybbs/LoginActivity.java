6f5a4a51ac4c117110eed347a859b124e33897dd
文锐 <wenrui199231@gmail.com>
2014-11-10 23:15:18 +0800

    Signed-off-by: 文锐 <wenrui199231@gmail.com>

--- lilybbs/src/info/nemoworks/lilybbs/LoginActivity.java
+++ lilybbs/src/info/nemoworks/lilybbs/LoginActivity.java
@@ -2,7 +2,6 @@ package info.nemoworks.lilybbs;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,20 +15,21 @@ public class LoginActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
+		this.setContentView(R.layout.activity_login);
 
-		Button b = (Button) findViewById(R.id.loginbutton);
+		Button b = (Button) this.findViewById(R.id.loginbutton);
 		b.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
-				EditText username = (EditText) findViewById(R.id.login);
-				EditText password = (EditText) findViewById(R.id.password);
+				EditText username = (EditText) LoginActivity.this.findViewById(R.id.login);
+				EditText password = (EditText) LoginActivity.this.findViewById(R.id.password);
 
 				if (username.getText().toString().length() > 0 && password.getText().toString().length() > 0) {
-					if (username.getText().toString().equals("test") && password.getText().toString().equals("test")) {
+					if (username.getText().toString().equals("wr") && password.getText().toString().equals("wr")) {
 
 						Intent intent = new Intent(LoginActivity.this, TopActivity.class);
-						startActivity(intent);
+						LoginActivity.this.startActivity(intent);
 					}
 				}
 			}
@@ -39,7 +39,7 @@ public class LoginActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
+		this.getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
 
